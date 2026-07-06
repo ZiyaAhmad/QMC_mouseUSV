4cb0a5b6583dca77a3ac833cdd562884821ab0b4
ZiyaAhmad <aziya286@gmail.com>
2026-07-06 15:59:36 -0700

    fixed issue w/ area_memb_max

--- glottal_area.m
+++ glottal_area.m
@@ -3,7 +3,7 @@ function glottal_area = glottal_area(PCAIA_activity, TA_activity)
 
 area_cart_max = 0.375e-6; % m^2 from CT scan measurment
 
-area_memb_max = 0.7e-6; % m^2 from CT scan measurment
+area_memb_max = 0.26e-6; % m^2 from CT scan measurment
 
 area_cart = area_cart_max * (1-PCAIA_activity);
 area_memb = area_memb_max * (1- TA_activity);
